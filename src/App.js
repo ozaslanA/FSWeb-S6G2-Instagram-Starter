5031fb28234f151700a66fd6032578bfd08a1970
ozaslanA <118729265+ozaslanA@users.noreply.github.com>
2023-08-30 13:14:31 +0300

    devam edilecek

--- src/App.js
+++ src/App.js
@@ -18,26 +18,29 @@ const App = () => {
   // Gönderi nesneleri dizisini tutmak için "gonderiler" adlı bir state oluşturun, **sahteVeri'yi yükleyin**.
   // Artık sahteVeri'ye ihtiyacınız olmayacak.
   // Arama çubuğunun çalışması için , arama kriterini tutacak başka bir state'e ihtiyacımız olacak.
-  const [aramaKriteri, setAramaKriteri] = useState("");
+  const [aramaKriteri, setAramaKriteri] = useState("asasasfasffas");
   const [gonderiler, setGonderiler] = useState(sahteVeri);
   const gonderiyiBegen = (gonderiID) => {
-    /*
-      Bu fonksiyon, belirli bir id ile gönderinin beğeni sayısını bir artırma amacına hizmet eder.
+    console.log("gonderiyiBegen tetiklendi", gonderiID);
 
-      Uygulamanın durumu, React ağacının en üstünde bulunur, ancak iç içe geçmiş bileşenlerin stateleri değiştirememesi adil olmaz!
-      Bu fonksiyon, belirli bir gönderinin beğeni sayısını artırılmasına olanak sağlamak amacıyla iç içe geçmiş bileşenlere aktarılır.
-
-	  "setGonderi" yi çağırın ve state ine "posts.map" çağrısını iletin.
-      `map` içine iletilen callback aşağıdaki mantığı gerçekleştirir:
-        - gönderinin idsi "gonderiID" ile eşleşirse, istenen değerlerle yeni bir gönderi nesnesi döndürün.
-        - aksi takdirde, sadece gönderi nesnesini değiştirmeden döndürün.
-     */
+    const gonderiyiBegen = (gonderiID) => {
+      setGonderiler(
+        gonderiler.map((item) => {
+          if (gonderiID === item.id) {
+            item.likes++;
+          }
+          return item;
+        })
+      );
+    };
   };
-
   return (
     <div className="App">
-      <AramaCubugu />
-      <Gonderiler />
+      <AramaCubugu term={aramaKriteri} setTermCB={setAramaKriteri} />
+      <Gonderiler
+        gonderilerProp={gonderiler}
+        gonderiyiBegenCB={gonderiyiBegen}
+      />
       {/* Yukarıdaki metni projeye başladığınızda silin*/}
       {/* AramaÇubuğu ve Gönderiler'i render etmesi için buraya ekleyin */}
       {/* Her bileşenin hangi proplara ihtiyaç duyduğunu kontrol edin, eğer ihtiyaç varsa ekleyin! */}
